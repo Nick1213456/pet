2fd90bceaf30282c42457c357963c87ce29c71aa
Nick <ez7755174@gmail.com>
2024-01-14 14:21:42 +0800

    忘記密碼視窗/登入按鈕bug修復

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,36 +1,68 @@
 $(document).ready(function () {
-    $(".login_modal").hide();
-    // 判斷登入登出顯示
-    $.post('/check-session', function(data) {
-        if (data===""){
+    checkLoginStatus();
+    // 监听 beforeunload 事件，页面即将被卸载时触发
+    $(window).on('beforeunload', function () {
+        // 在强制刷新页面时重新检查登录状态
+        checkLoginStatus();
+    });
+});
+// 封裝檢查是否登入
+function checkLoginStatus() {
+    $(".login_modal").hide(); //隱藏登出
+    $.post('/check-session', function (data) {
+        if (data === "") {
             //如果沒登入
+            console.log("沒登入")
             $(".login_bar").show();
             $(".d-none1").hide();
-        }else {
+        } else {
+            console.log("有登入")
             $(".login_bar").hide();
             $(".d-none1").show();
         }
-
     });
-    $(".d-none1").on('click',function(){
-        alert("會員已登出 將返回首頁")
-    })
-});
+}
+
+$("#d-none2").on('click', function () {
+    alert("會員已登出 將返回首頁")
+})
 
 $(document).on('click', function (e) {
 
     var modal = $(".login_bar_up");
     if (e.target.className == $(".login_bar").attr("class")) {
-        $(".login_modal").show();
-        $('a').css('pointer-events', 'none');
-        $('.login_text2').css('pointer-events', 'auto');
+        $.post('/check-session', function (data) {
+            if (data === "") {
+                //如果沒登入
+                $(".login_modal").show();
+                $('a').css('pointer-events', 'none');
+                $('.login_text2').css('pointer-events', 'auto');
+            } else {
+                console.log("有登入")
+                alert("您已登入")
+                $(".login_bar").hide();
+                $(".d-none1").show();
+            }
+        });
+
+
+
 
     } else if (!modal.is(e.target) && modal.has(e.target).length === 0) {
-        // 將彈出框隱藏                
+        // 將彈出框隱藏
         $(".login_modal").hide();
         $('a').css('pointer-events', 'auto');
     }
 
 
+});
 
+
+$("#forget_password2_2").click(function () {
+    $("#forget_password3").show();
+    $("#login_modal_2").hide();
+});
+$("#login_up2").click(function () {
+    $("#forget_password3").hide();
+    $("#login_modal_2").show();
 });
